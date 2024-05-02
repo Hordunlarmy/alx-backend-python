92518783e813ec35a870171f0b4cfe6aa7beee80
hordunlarmy <hordunlarmy@gmail.com>
2024-05-02 14:53:17 +0000

    Update function signature to use List from typing module

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -3,8 +3,10 @@
 a type-annotated function which takes a list of floats as argument
 """
 
+from typing import List
 
-def sum_list(input_list: list[float]) -> float:
+
+def sum_list(input_list: List[float]) -> float:
     """ returns their sum as a float. """
 
     return sum(input_list)
