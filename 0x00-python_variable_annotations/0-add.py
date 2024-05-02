a89ccadd171bb9795a33fb6004441a489d65de9a
hordunlarmy <hordunlarmy@gmail.com>
2024-05-02 14:22:34 +0000

    Add initial Python variable annotations for add function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+a type-annotated function add that takes a float a and a float b as arguments
+"""
+
+
+def add(a: float, b: float) -> float:
+    """ returns the sum of a and b as a float """
+    return a + b
