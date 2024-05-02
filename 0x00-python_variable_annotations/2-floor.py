73cf18a6cdac7b15b525c4ab890569df36ed8b1a
hordunlarmy <hordunlarmy@gmail.com>
2024-05-02 14:30:52 +0000

    Add function floor with type annotations and tests

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+a type-annotated function floor which takes a float n as argument
+"""
+
+
+def floor(a: float) -> int:
+    """ returns the floor of the float. """
+
+    return int(a)
