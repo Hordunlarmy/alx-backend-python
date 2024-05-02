3c0b3c9113e3e52a0c11bf575ab31ef41780f246
hordunlarmy <hordunlarmy@gmail.com>
2024-05-02 15:02:17 +0000

    Add script to convert key-value pairs to tuple

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+a type-annotated function to_kv that takes a string k and an int OR float v as
+arguments
+"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    returns a tuple. The first element of the tuple is the string k.
+    The second element is the square of the int/float v
+    """
+
+    return (k, float(v**2))
