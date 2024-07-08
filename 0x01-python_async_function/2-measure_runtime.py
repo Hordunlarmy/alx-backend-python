d5b9f005cc0fd8385e1dcda43bd6988870f77086
Hordunlarmy <Hordunlarmy@gmail.com>
2024-07-09 00:29:59 +0100

    fix: Change `measure_time` to return float instead of async

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -7,7 +7,7 @@ from asyncio import run
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """ Measure the runtime of wait_n """
 
     start = time.time()
